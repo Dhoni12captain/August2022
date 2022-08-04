359fef7b61878366c9fdd74a90b16ce3859a61cd
Dhoni12captain <sanchintu1212@gmail.com>
2022-08-04 14:52:48 +0530

    Aug1

---
+++ src/org/sample/CompanyInfo.java
@@ -0,0 +1,29 @@
+package org.sample;
+
+public class CompanyInfo {
+public static void main(String[] args) {
+	// Datatype variableName = value;
+	byte age = 20;
+	System.out.println("Employee age is :" +age);
+	short atmPin = 1234;
+	System.out.println("Employee atmpin is :" +atmPin);
+	int empId = 8764536;
+	System.out.println("Employee id is :" +empId);
+	long mobile = 8675340542l;
+	System.out.println("Employee Mobile No. is :" +mobile);
+	float salary = 46566.4567f;
+	System.out.println("Employee salary is :" +salary);
+	double salary2 = 3567878.245465d;
+	System.out.println("Employee second salary is :" +salary2);
+	char gender = 'M';
+	System.out.println("Employee Gender is :" +gender);
+	String email = "sanchintu1212@gmail.com";
+	System.out.println("Employee email is :" +email);
+	boolean maritalStatus = false;
+	System.out.println("Employee marital status is :" +maritalStatus);
+	float a = 20/3f;
+	System.out.println(a);
+	double b = 20/3d;
+	System.out.println(b);
+}
+}
